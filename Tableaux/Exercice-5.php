aa9cd7d5e917e8f53d77c13926a0c98a265f0560
[olivier-keutgens] <[keutgensolivier@gmail.com]>
2018-12-03 13:57:50 +0100

    série Tableaux, done

---
+++ Tableaux/Exercice-5.php
@@ -0,0 +1,3 @@
+<?php
+$depa = ["01" => "Ain"; "03" => "Allier"; "07" => "Ardèche"; "15" => "Cantak"; "26" => "Drôme"; "38" => "Isère"; "42" => "Loire"; "43" => "Haute-Loire"; "63" => "Puy-de-Dôme"; "69D" => "Rhône"; "69M" => "Méropole de Lyon"; "73" => "Savoie"; "74" => "Haute-Savoie"];
+?>
